0d17245789f67800329529c4558fa40fd2a0fcec
speedyhoon <speedyhoon@users.noreply.github.com>
2016-01-09 23:06:36 +1100

    Rename variables

--- hotson.js
+++ hotson.js
@@ -1,38 +1,39 @@
-   function render(element, jsonData){
-      switch(true){
-      case Array.isArray(jsonData):
-         var item, old = element.cloneNode(true), frag = document.createDocumentFragment();
-         for(item in jsonData){
-            if(jsonData.hasOwnProperty(item)){
-               frag.appendChild(loopy(old, jsonData[item]));
-            }
-         }
-         element.parentNode.replaceChild(frag, element);
-         return;
-      case typeof jsonData === "object":
-         element.parentNode.replaceChild(loopy(element, jsonData), element);
-      }
-   }
+	function render(element, jsonData){
+		switch(true){
+		case Array.isArray(jsonData):
+			var item, old = element.cloneNode(true), fragment = document.createDocumentFragment();
+			for(item in jsonData){
+				if(jsonData.hasOwnProperty(item)){
+					fragment.appendChild(loopy(old, jsonData[item]));
+				}
+			}
+			element.parentNode.replaceChild(fragment, element);
+			return;
+		case typeof jsonData === "object":
+			element.parentNode.replaceChild(loopy(element, jsonData), element);
+		}
+	}
 
-   function loopy(old, temp){
-      var attr, ele, newCopy = old.cloneNode(true);
-      for(attr in temp){
-         if(temp.hasOwnProperty(attr) && (ele = newCopy.querySelector('[name=' + attr + '], .' + attr+ ', #' + attr))) {
-            if(typeof temp[attr] === "object" && !Array.isArray(temp[attr])){
-               render(ele, temp[attr]);
-            }else {
-               assignText(ele, temp[attr]);
-            }
-         }
-      }
-      return newCopy;
-   }
+	function loopy(parentElement, jsonData){
+		var attr, element;
+		parentElement = parentElement.cloneNode(true);
+		for(attr in jsonData){
+			if(jsonData.hasOwnProperty(attr) && (element = parentElement.querySelector('[name=' + attr + '], .' + attr+ ', #' + attr+', [data-h=' + attr + ']'))) {
+				if(typeof jsonData[attr] === "object" && !Array.isArray(jsonData[attr])){
+					render(element, jsonData[attr]);
+				}else {
+					assignText(element, jsonData[attr]);
+				}
+			}
+		}
+		return parentElement;
+	}
 
-   function assignText(element, value){
-      switch(element.tagName){
-      case "INPUT":
-         element.value = value;
-         return;
-      }
-      element.textContent = value;
-   }
+	function assignText(element, value){
+		switch(element.tagName){
+		case "INPUT":
+			element.setAttribute('value', value);
+			return;
+		}
+		element.textContent = value;
+	}
