09b59cca72456b066294cbc7896a01e46b02e399
speedyhoon <speedyhoon@users.noreply.github.com>
2016-07-07 00:51:34 +1000

    Update mindjholts.js
    Add support for :nth-child selectors using JavaScript keys that are integers of any variable type.
    Condense code & add some comments

--- mindjholts.js
+++ mindjholts.js
@@ -1,39 +1,45 @@
-	function render(element, jsonData){
-		switch(true){
-		case Array.isArray(jsonData):
+var m = {
+	render: function (element, jsonData){
+		if(Array.isArray(jsonData)){
 			var item, old = element.cloneNode(true), fragment = document.createDocumentFragment();
 			for(item in jsonData){
 				if(jsonData.hasOwnProperty(item)){
-					fragment.appendChild(loopy(old, jsonData[item]));
+					fragment.appendChild(m.loopy(old, jsonData[item]));
 				}
 			}
 			element.parentNode.replaceChild(fragment, element);
-			return;
-		case typeof jsonData === "object":
-			element.parentNode.replaceChild(loopy(element, jsonData), element);
+		}else if(typeof jsonData === 'object'){
+			element.parentNode.replaceChild(m.loopy(element, jsonData), element);
 		}
-	}
-
-	function loopy(parentElement, jsonData){
-		var attr, element;
+	},
+	loopy: function(parentElement, jsonData){
+		var querySelector, element;
 		parentElement = parentElement.cloneNode(true);
-		for(attr in jsonData){
-			if(jsonData.hasOwnProperty(attr) && (element = parentElement.querySelector('[name=' + attr + '], .' + attr+ ', #' + attr+', [data-h=' + attr + ']'))) {
-				if(typeof jsonData[attr] === "object" && !Array.isArray(jsonData[attr])){
-					render(element, jsonData[attr]);
-				}else {
-					assignText(element, jsonData[attr]);
+		for(var attr in jsonData){
+			if(jsonData.hasOwnProperty(attr)){
+				//Check if attribute is an integer. Not using typeof here because Javascript converts integer object keys to strings.
+				if(attr === ~~attr + ''){
+					querySelector = ':nth-child(' + (~~attr + 1) + ')';
+				}else{
+					querySelector = '#' + attr + ', .' + attr + ', [name=' + attr + ']';
+				}
+				if(element = parentElement.querySelector(querySelector)){
+					if(typeof jsonData[attr] === 'object' && !Array.isArray(jsonData[attr])){
+						m.render(element, jsonData[attr]);
+					}else{
+						m.assignValue(element, jsonData[attr]);
+					}
 				}
 			}
 		}
 		return parentElement;
-	}
-
-	function assignText(element, value){
+	},
+	assignValue: function(element, value){
 		switch(element.tagName){
-		case "INPUT":
+		case 'INPUT':
 			element.setAttribute('value', value);
 			return;
 		}
 		element.textContent = value;
 	}
+};
